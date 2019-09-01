b97d4f59eadd4d77e3ff25c11bbfe4d0b92c5c57
Fffh <157>
2019-09-01 16:40:16 +0800

    11

--- src/com/briup/jd1911/hello.java
+++ src/com/briup/jd1911/hello.java
@@ -1,5 +1,48 @@
 package com.briup.jd1911;
 
 public class hello {
+    private String name;
+    private int age;
 
+    public hello(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "hello{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        String s = "第一次使用";
+        int s2=1;
+        System.out.println(s);
+        /*
+
+         */
+        /**
+         *
+         */
+
+    }
 }
